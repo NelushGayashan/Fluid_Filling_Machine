a39a1861bd0330e094458a3d47cb4a9fd3de0198
NGFXavier <nelushgayashan@gmail.com>
2024-07-20 22:52:31 +0530

    Adding delay functions

---
+++ src/Delay.c
@@ -0,0 +1,26 @@
+#include "Delay.h"
+
+void delay1()
+{
+    unsigned char i, j;
+    for (i = 0; i < 20; i++)
+    {
+        for (j = 0; j < 255; j++)
+        {
+        }
+    }
+}
+
+void delay2()
+{
+    unsigned char i, j, k;
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 255; j++)
+        {
+            for (k = 0; k < 255; k++)
+            {
+            }
+        }
+    }
+}
---
+++ src/Delay.h
@@ -0,0 +1,7 @@
+#ifndef DELAY_H
+#define DELAY_H
+
+void delay1();
+void delay2();
+
+#endif // DELAY_H
